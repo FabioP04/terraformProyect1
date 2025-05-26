176dc98d3afc956e92a754f9da4c818c43162543
FabioP04 <fabioperezp04@gmail.com>
2025-05-26 13:57:25 +0200

    changed resource names on outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 # Data source to access the properties of an existing Azure Public IP Address
-data "azurerm_public_ip" "tfexample" {
-  name                = azurerm_public_ip.tfexample.name
-  resource_group_name = azurerm_linux_virtual_machine_scale_set.tfexample.resource_group_name
+data "azurerm_public_ip" "web_cluster_public_ip_data" {
+  name                = azurerm_public_ip.web_cluster_public_ip.name
+  resource_group_name = azurerm_resource_group.web_cluster_rg.name
 }
 
-# # Output variable: Public IP address
+# Output variable: Public IP address
 output "public_ip" {
-  value = data.azurerm_public_ip.tfexample.ip_address
+  value = data.azurerm_public_ip.web_cluster_public_ip_data.ip_address
 }
