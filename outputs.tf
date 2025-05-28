8f4c9b62d882ec59c86f42aefe77de36e7b9e652
FabioP04 <fabioperezp04@gmail.com>
2025-05-28 14:06:52 +0200

    stable outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,11 @@
+
+# Data source to access the properties of an existing Azure Public IP Address
 data "azurerm_public_ip" "web_cluster_public_ip_data" {
   name                = azurerm_public_ip.web_cluster_public_ip.name
   resource_group_name = azurerm_resource_group.web_cluster_rg.name
 }
 
-output "vmss_public_ip" {
-  description = "Public IP address of the load balancer for the VMSS"
-  value       = data.azurerm_public_ip.web_cluster_public_ip_data.ip_address
-}
-
-output "monitoring_vm_public_ip" {
-  description = "Public IP address of the monitoring VM"
-  value       = azurerm_public_ip.monitoring_public_ip.ip_address
+# Output variable: Public IP address
+output "public_ip" {
+  value = data.azurerm_public_ip.web_cluster_public_ip_data.ip_address
 }
