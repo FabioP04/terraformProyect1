f3b16f3b8aab52411cdf6a5b60a73b719ac8b24b
FabioP04 <fabioperezp04@gmail.com>
2025-05-28 11:18:56 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-# Data source to access the properties of an existing Azure Public IP Address
 data "azurerm_public_ip" "web_cluster_public_ip_data" {
   name                = azurerm_public_ip.web_cluster_public_ip.name
   resource_group_name = azurerm_resource_group.web_cluster_rg.name
