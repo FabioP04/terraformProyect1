da1ca4f7528dffea1d1013a802de54a9faeeda9f
FabioP04 <fabioperezp04@gmail.com>
2025-05-28 11:39:03 +0200

    Update mon outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,15 @@ data "azurerm_public_ip" "web_cluster_public_ip_data" {
   resource_group_name = azurerm_resource_group.web_cluster_rg.name
 }
 
-# Output variable: Public IP address
 output "public_ip" {
   value = data.azurerm_public_ip.web_cluster_public_ip_data.ip_address
+}
+
+data "azurerm_public_ip" "monitoring_public_ip_data" {
+  name                = azurerm_public_ip.monitoring_public_ip.name
+  resource_group_name = azurerm_resource_group.web_cluster_rg.name
+}
+
+output "monitoring_public_ip" {
+  value = data.azurerm_public_ip.monitoring_public_ip_data.ip_address
 }
