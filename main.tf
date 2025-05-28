17a1c8ee37a403152110b00749588fa048ae40c4
FabioP04 <fabioperezp04@gmail.com>
2025-05-28 13:22:06 +0200

    Update ssh main.tf

--- main.tf
+++ main.tf
@@ -38,6 +38,40 @@ resource "azurerm_subnet" "web_cluster_subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_network_security_group" "monitoring_nsg" {
+  name                = "monitoring-nsg"
+  location            = azurerm_resource_group.web_cluster_rg.location
+  resource_group_name = azurerm_resource_group.web_cluster_rg.name
+
+  security_rule {
+    name                       = "Allow-Prometheus"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9090"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-Grafana"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "my-terraform-env"
+  }
+}
+
 resource "azurerm_public_ip" "web_cluster_public_ip" {
   name                = "my-terraform-public-ip"
   location            = azurerm_resource_group.web_cluster_rg.location
@@ -76,14 +110,14 @@ resource "azurerm_lb_probe" "web_cluster_lb_probe" {
 }
 
 resource "azurerm_lb_rule" "web_cluster_lb_rule" {
-  name                            = "my-terraform-lb-rule"
-  loadbalancer_id                 = azurerm_lb.web_cluster_lb.id
-  protocol                        = "Tcp"
-  frontend_port                   = 80
-  backend_port                    = var.server_port
-  frontend_ip_configuration_name  = "my-terraform-lb-frontend-ip"
-  backend_address_pool_ids        = [azurerm_lb_backend_address_pool.web_cluster_lb_backend_pool.id]
-  probe_id                       = azurerm_lb_probe.web_cluster_lb_probe.id
+  name                           = "my-terraform-lb-rule"
+  loadbalancer_id                = azurerm_lb.web_cluster_lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = var.server_port
+  frontend_ip_configuration_name = "my-terraform-lb-frontend-ip"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.web_cluster_lb_backend_pool.id]
+  probe_id                      = azurerm_lb_probe.web_cluster_lb_probe.id
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "web_cluster_vmss" {
@@ -155,6 +189,7 @@ resource "azurerm_network_interface" "monitoring_nic" {
   name                = "monitoring-nic"
   location            = azurerm_resource_group.web_cluster_rg.location
   resource_group_name = azurerm_resource_group.web_cluster_rg.name
+  network_security_group_id = azurerm_network_security_group.monitoring_nsg.id
 
   ip_configuration {
     name                          = "internal"
@@ -168,60 +203,13 @@ resource "azurerm_network_interface" "monitoring_nic" {
   }
 }
 
-resource "azurerm_network_security_group" "monitoring_nsg" {
-  name                = "monitoring-nsg"
-  location            = azurerm_resource_group.web_cluster_rg.location
-  resource_group_name = azurerm_resource_group.web_cluster_rg.name
-
-  security_rule {
-    name                       = "Allow-Prometheus"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "9090"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-Grafana"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3000"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-SSH"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "monitoring_nic_nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.monitoring_nic.id
-  network_security_group_id = azurerm_network_security_group.monitoring_nsg.id
-}
-
 resource "azurerm_linux_virtual_machine" "monitoring_vm" {
-  name                = "monitoring-vm"
-  location            = azurerm_resource_group.web_cluster_rg.location
-  resource_group_name = azurerm_resource_group.web_cluster_rg.name
-  size                = "Standard_DS1_v2"
-  admin_username      = "azureuser"
-  admin_password      = "Password1234!"
+  name                            = "monitoring-vm"
+  location                        = azurerm_resource_group.web_cluster_rg.location
+  resource_group_name             = azurerm_resource_group.web_cluster_rg.name
+  size                            = "Standard_DS1_v2"
+  admin_username                 = "azureuser"
+  admin_password                 = "Password1234!"
   disable_password_authentication = false
 
   network_interface_ids = [azurerm_network_interface.monitoring_nic.id]
@@ -242,20 +230,24 @@ resource "azurerm_linux_virtual_machine" "monitoring_vm" {
     environment = "my-terraform-env"
   }
 
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "azureuser"
-      password = "Password1234!"
-      host     = azurerm_public_ip.monitoring_public_ip.ip_address
-    }
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install -y prometheus grafana",
-      "sudo systemctl enable prometheus",
-      "sudo systemctl start prometheus",
-      "sudo systemctl enable grafana-server",
-      "sudo systemctl start grafana-server"
-    ]
-  }
+  # Instala Prometheus y Grafana usando VM Extension
+}
+
+resource "azurerm_virtual_machine_extension" "monitoring_vm_extension" {
+  name                 = "install-prometheus-grafana"
+  virtual_machine_id   = azurerm_linux_virtual_machine.monitoring_vm.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.1"
+
+  settings = <<SETTINGS
+{
+  "commandToExecute": "sudo apt-get update && sudo apt-get install -y wget apt-transport-https software-properties-common && \
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add - && \
+echo 'deb https://packages.grafana.com/oss/deb stable main' | sudo tee -a /etc/apt/sources.list.d/grafana.list && \
+sudo apt-get update && sudo apt-get install -y grafana prometheus && \
+sudo systemctl enable prometheus && sudo systemctl start prometheus && \
+sudo systemctl enable grafana-server && sudo systemctl start grafana-server"
+}
+SETTINGS
 }
